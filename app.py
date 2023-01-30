8455611e501c29ae42be565dfb458520e596e306
Himzel Washington <0xhimzel@users.noreply.huggingface.co>
2023-01-30 17:51:14 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 import gradio as gr
 
-gr.Interface.load("models/Hello-SimpleAI/chatgpt-detector-roberta", title="🤖 Detect AI Plagiarism").launch()
+gr.Interface.load("models/Hello-SimpleAI/chatgpt-detector-roberta",
+                  title="🤖 Detect AI Plagiarism",
+                 description= "This model is trained on **the mix of full-text and splitted sentences** of `answer`s from [Hello-SimpleAI/HC3](https://huggingface.co/datasets/Hello-SimpleAI/HC3). More details refer to [arxiv: 2301.07597](https://arxiv.org/abs/2301.07597) and Gtihub project [Hello-SimpleAI/chatgpt-comparison-detection](https://github.com/Hello-SimpleAI/chatgpt-comparison-detection). The base checkpoint is [roberta-base](https://huggingface.co/roberta-base). We train it with all [Hello-SimpleAI/HC3](https://huggingface.co/datasets/Hello-SimpleAI/HC3) data (without held-out) for 1 epoch. (1-epoch is consistent with the experiments in [our paper](https://arxiv.org/abs/2301.07597)").launch()
