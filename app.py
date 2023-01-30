f2e0115313c0ff6cf89191f52959dc41f3210e15
Himzel Washington <0xhimzel@users.noreply.huggingface.co>
2023-01-30 17:35:43 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 import gradio as gr
 
-gr.Interface.load("models/Hello-SimpleAI/chatgpt-detector-roberta", title="🤖 Detect AI Plagiarism", input="hello world").launch()
+gr.Interface.load("models/Hello-SimpleAI/chatgpt-detector-roberta", title="🤖 Detect AI Plagiarism").launch()
